af72cbae51dc7ef215c5062436a66c351f715fcb
Bethany Shao <bethany.magdalena@gmail.com>
2017-10-17 20:22:31 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   words = string.split(' ')
-  reversed = string.split('').collect { |word| word.reverse }
+  reversed = words.collect { |word| word.reverse }
   reversed.join(' ')
 end
